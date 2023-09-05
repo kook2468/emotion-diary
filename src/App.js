e9ed68a05be11a12be1fd4df9aa6473423cc441e
da gyeong choi <cdg9808@naver.com>
2023-09-05 16:58:21 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,122 @@
+import React, { useEffect, useReducer, useRef } from "react";
+
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Home from "./pages/Home";
+import New from "./pages/New";
+import Diary from "./pages/Diary";
+import Edit from "./pages/Edit";
+
+const reducer = (state, action) => {
+  let newState = [];
+  switch (action.type) {
+    case "INIT": {
+      return action.data;
+    }
+    case "CREATE": {
+      console.log("CREATE");
+      console.log(action.data);
+      newState = [action.data, ...state];
+      break;
+    }
+    case "REMOVE": {
+      newState = state.filter((it) => it.id !== action.targetId);
+      break;
+    }
+    case "EDIT": {
+      console.log(action.data);
+      newState = state.map((it) =>
+        it.id === action.data.id ? { ...action.data } : it
+      );
+      break;
+    }
+    default:
+      return state;
+  }
+
+  localStorage.setItem("diary", JSON.stringify(newState));
+  return newState;
+};
+
+export const DiaryStateContext = React.createContext();
+export const DiaryDispatchContext = React.createContext();
+
+function App() {
+  const [data, dispatch] = useReducer(reducer, []);
+
+  useEffect(() => {
+    const localData = localStorage.getItem("diary");
+    if (localData) {
+      const diaryList = JSON.parse(localData).sort(
+        (a, b) => parseInt(b.id) - parseInt(a.id) //산술 연산시 parseInt 습관화
+      );
+
+      if (diaryList.length > 0) {
+        //범위 벗어나는 인덱스에 접근하려고 하면 에러남 : 빈배열([])일 경우
+        dataId.current = diaryList[0].id + 1;
+      }
+
+      console.log(diaryList);
+      console.log(dataId);
+
+      dispatch({ type: "INIT", data: diaryList });
+    }
+  }, []);
+
+  const dataId = useRef(0);
+
+  // CREATE
+  const onCreate = (date, content, emotion) => {
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: dataId.current,
+        date: new Date(date).getTime(),
+        content,
+        emotion,
+      },
+    });
+    dataId.current += 1;
+  };
+
+  // REMOVE
+  const onRemove = (targetId) => {
+    dispatch({ type: "REMOVE", targetId });
+  };
+
+  // EDIT
+  const onEdit = (targetId, date, content, emotion) => {
+    dispatch({
+      type: "EDIT",
+      data: {
+        id: targetId,
+        date: new Date(date).getTime(),
+        content,
+        emotion,
+      },
+    });
+  };
+
+  const env = process.env;
+  env.PUBLIC_URL = env.PUBLIC_URL || "";
+
+  return (
+    <DiaryStateContext.Provider value={data}>
+      <DiaryDispatchContext.Provider value={{ onCreate, onEdit, onRemove }}>
+        <BrowserRouter>
+          <div className="App">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/new" element={<New />} />
+              <Route path="/edit/:id" element={<Edit />} />
+              <Route path="/diary/:id" element={<Diary />} />
+            </Routes>
+          </div>
+        </BrowserRouter>
+      </DiaryDispatchContext.Provider>
+    </DiaryStateContext.Provider>
+  );
+}
+
+export default App;
---
+++ src/pages/Diary.js
@@ -0,0 +1,90 @@
+import { useContext, useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom"; //custom Hooks
+import { DiaryStateContext } from "../App";
+import { getStringDate } from "../util/date";
+import { emotionList } from "../util/emotion";
+
+import MyButton from "../components/MyButton";
+import MyHeader from "../components/MyHeader";
+
+const Diary = () => {
+  const { id } = useParams();
+  const diaryList = useContext(DiaryStateContext); //context가 가진 diaryList에서 params id로 값 추출
+  const navigate = useNavigate();
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    const titleElement = document.getElementsByTagName("title")[0];
+    titleElement.innerHTML = `감성 일기장 - ${id}번 일기`;
+  }, []);
+
+  useEffect(() => {
+    if (diaryList.length >= 1) {
+      const targetDiary = diaryList.find(
+        (it) => parseInt(it.id) === parseInt(id)
+      );
+      console.log(targetDiary);
+
+      if (targetDiary) {
+        //일기 존재할 때
+        setData(targetDiary);
+      } else {
+        //일기 없을 때
+        alert("없는 일기입니다.");
+        navigate("/", { replace: true });
+      }
+    }
+  }, [id, diaryList]);
+
+  if (!data) {
+    return <div className="DiaryPage">로딩중입니다...</div>;
+  } else {
+    const curEmotionData = emotionList.find(
+      (it) => parseInt(it.emotion_id) === parseInt(data.emotion)
+    );
+
+    console.log(curEmotionData.emotion_descript);
+
+    return (
+      <div className="DiaryPage">
+        <MyHeader
+          headText={`${getStringDate(new Date(data.date))} 기록`}
+          leftChild={
+            <MyButton text={"< 뒤로가기"} onClick={() => navigate(-1)} />
+          }
+          rightChild={
+            <MyButton
+              text={"수정하기"}
+              onClick={() => navigate(`/edit/${data.id}`)}
+            />
+          }
+        />
+        <article>
+          <section>
+            <h4> 오늘의 감정 </h4>
+            <div
+              className={[
+                "diary_img_wrapper",
+                `diary_img_wrapper_${data.emotion}`,
+              ].join(" ")}
+            >
+              <img src={curEmotionData.emotion_img} />
+              <div className="emotion_descript">
+                {curEmotionData.emotion_descript}
+              </div>
+            </div>
+          </section>
+
+          <section>
+            <h4> 오늘의 일기 </h4>
+            <div className="diary_content_wrapper">
+              <p>{data.content}</p>
+            </div>
+          </section>
+        </article>
+      </div>
+    );
+  }
+};
+
+export default Diary;
